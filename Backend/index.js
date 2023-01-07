c1d48cfb999c8d563ca7f55816763d821c364807
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-01-07 19:36:26 +0530

    initial project commit

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const app = express();
+
+
+
+
+
+app.listen(3000, ()=>{
+    console.log(`server started successfully at port : ${3000} `);
+})
