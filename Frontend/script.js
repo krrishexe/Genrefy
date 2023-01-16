439afaddcdebf41d2b4f4ec395cba1534d84f894
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-01-16 20:26:42 +0530

    added json file to the frontend folder

--- Frontend/script.js
+++ Frontend/script.js
@@ -11,3 +11,36 @@ $(".navT").on("click", function(){
   }
 
 
+let http = new XMLHttpRequest();
+
+http.open('get',"./json-cleaner.json",true); // here true sets the keyword to be async
+
+http.send();
+http.onload = function (){
+  
+  if(this.readyState == 4 && this.status == 200){
+
+    let songs = JSON.parse(this.responseText);
+    let output = "";
+
+
+    for(let song of songs){
+      output += `
+      
+      <div class = "songs">
+
+
+      <p class="songName" style=${song.results.data.style}> ${song.results.text} </p>
+
+
+      </div>
+      `;
+      console.log(song.text);
+    }
+
+    document.querySelector(".songs").innerHTML = output;
+
+
+  }
+
+}
