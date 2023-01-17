a1f8b441a40201b261065b85bd960ac189aa8574
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-01-17 09:26:19 +0530

    rendered the songs on frontend

--- Frontend/script.js
+++ Frontend/script.js
@@ -11,36 +11,62 @@ $(".navT").on("click", function(){
   }
 
 
-let http = new XMLHttpRequest();
 
-http.open('get',"./json-cleaner.json",true); // here true sets the keyword to be async
+  function Func() {
+    fetch("./json-cleaner.json")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
 
-http.send();
-http.onload = function (){
+      for(let song of data["results"].slice(0,100)){
+
+        document.getElementById("songs").innerHTML += 
   
-  if(this.readyState == 4 && this.status == 200){
-
-    let songs = JSON.parse(this.responseText);
-    let output = "";
-
-
-    for(let song of songs){
-      output += `
+        `<div class = "song">
+  
+  
+               <a class="songName"> ${song.text} </a>
+        
+         
+        </div>`;
+      }
       
-      <div class = "songs">
+    }) 
+  };
+  
+  Func();
+
+// let http = new XMLHttpRequest();
+
+// http.open('get',"./json-cleaner.json",true); // here true sets the keyword to be async
+
+// http.send();
+// http.onload = function (){
+  
+//   if(this.readyState == 4 && this.status == 200){
+
+//     let songs = JSON.parse(this.responseText);
+//     let output = "";
 
 
-      <p class="songName" style=${song.results.data.style}> ${song.results.text} </p>
+//     for(let song of songs){
+//       output += `
+      
+//       <div class = "songs">
 
 
-      </div>
-      `;
-      console.log(song.text);
-    }
-
-    document.querySelector(".songs").innerHTML = output;
+//       <p class="songName" style=${song.results.data.style}> ${song.results.text} </p>
 
 
-  }
+//       </div>
+//       `;
+//       console.log(song.text);
+//     }
 
-}
+//     document.querySelector(".songs").innerHTML = output;
+
+
+//   }
+
