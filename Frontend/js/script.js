e5007d61acfdfc0c5d9d25cd87d82e00dd42f802
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-01-29 12:29:01 +0530

    added scroll to top btn

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -7,16 +7,16 @@ $(".navT").on("click", function () {
 $(document).ready(function () {
   'use strict';
   $('button').on('click', function () {
-     $('html').animate({
-         scrollTop: 0
-     }, 1000);
+    $('html').animate({
+      scrollTop: 0
+    }, 1000);
   });
   $(window).on('scroll', function () {
-     if ($(window).scrollTop() > 400) {
-         $('button').fadeIn();
-     } else {
-         $('button').fadeOut();
-     }
+    if ($(window).scrollTop() > 600) {
+      $('button').fadeIn();
+    } else {
+      $('button').fadeOut();
+    }
   });
 });
 
@@ -38,13 +38,13 @@ function Func() {
   fetch("../json-cleaner.json")       // http://localhost:3000/api , {mode:'no-cors}
     .then((res) => {
       // console.log(res.json());
-       return res.json();
+      return res.json();
     })
     .then((data) => {
       console.log(data);
       for (let song of data["results"].slice(0, 100)) {
         const arrColors = ["#0081C9", "#5BC0F8", "#86E5FF", "#FFC93C", "#FF7B54", "#FF78F0", "#C780FA", "#FAD3E7", "#ECECEC", "#FF0032"]
-        const arrFonts = ['Aboreto','Explora','Unbounded','Libre Baskerville','Dancing Script','Indie Flower','Bebas Neue',]
+        const arrFonts = ['Aboreto', 'Explora', 'Unbounded', 'Libre Baskerville', 'Dancing Script', 'Indie Flower', 'Bebas Neue',]
         var colors = Math.floor(Math.random() * arrColors.length);
         var x = Math.random();
         var y = x * 100;
@@ -60,7 +60,7 @@ function Func() {
         }
         document.getElementById("songs").innerHTML +=
 
-      `
+          `
       <div onclick = 'playBoy(\"${song.data.preview_url}\","${song.data.id}")'>
              <a id= "${song.data.id}" class="songName" style = "width:100%; display:grid; font-family:${arrFonts[colors]}; color:${arrColors[colors]} ;writing-mode: horizontal-lr;font-size:${z}px ;float:${arr[a]}"> ${(song.text).slice(0, -2)} ▶
              </a> 
